b29cb27adba9c338dab129c2be3e88cab6759d65
karim kohel <kareemkohel@gmail.com>
2021-03-21 23:48:59 +0200

    handling for failed pipelines to restart

--- sender.py
+++ sender.py
@@ -12,7 +12,7 @@ Gst.init(sys.argv[1:])
 
 def mainloop(pipes):
     while True:
-        sleep(1)
+        sleep(3)
         checkStates(pipes)
 
 def isVideo(element):
@@ -23,10 +23,9 @@ def cleanPipelins(pipelines):
         pipe.set_state(Gst.State.NULL)
         print("- pipeline " + str(i) + " resetting to null")
 
-def startPipes():
+def getCamsAndPipes():
     cams = filter(isVideo, os.listdir("/dev/")) # gets list of video devices if isVideo finds them in os list
     pipelines = []
-    print(cams)
 
     for i, cam in enumerate(cams):
         pipStr = 'v4l2src device="/dev/{}" !  video/x-raw,width=640,height=480 ! jpegenc ! rtpjpegpay !  udpsink host=192.168.2.255 port=5{}00'.format(cam, i)
@@ -39,22 +38,25 @@ def startPipes():
             print("pipeline error")
             sys.exit(1)
 
+    return pipelines
+
+def startPipes(pipelines = getCamsAndPipes()):
+
     for i, pipeline in enumerate(pipelines):
         ret = pipeline.set_state(Gst.State.PLAYING)
         if ret == Gst.StateChangeReturn.FAILURE:
-            print("Unable to set pipeline {} to the playing state.".format(i))
+            print("pipeline {} couldn't started.".format(i))
         else:
             print("pipeline {} started.".format(i))
 
     return pipelines
 
-
 def checkStates(pipelines):
     for pipeline in pipelines:
         _, state, _ = pipeline.get_state(timeout=10*Gst.SECOND)
         if state != Gst.State.PLAYING:
-            # startPipelines([pipeline])
-            print("Pipline stopped !!!!!!")
+            print("Pipline stopped. Restarting...")
+            startPipes([pipeline])
             break
 
 
@@ -62,13 +64,12 @@ main_loop = GLib.MainLoop()
 thread = Thread(target=main_loop.run, daemon=True)
 thread.start()
 
-
 pipes = startPipes()
 
 try:
     mainloop(pipes)
 except KeyboardInterrupt:
-    print("exiting")
+    print("exiting...\n")
     pass
 
 
