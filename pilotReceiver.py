cb62ba77078847ca98a90cee418b1bd226488448
karim kohel <kareemkohel@gmail.com>
2021-04-04 22:44:43 +0200

    dependency hell loop

--- pilotReceiver.py
+++ pilotReceiver.py
@@ -1,10 +1,13 @@
 import cv2
 
 
-cam = cv2.VideoCapture('udpsrc port=5000 !  application/x-rtp, encoding-name=JPEG,payload=26 !  rtpjpegdepay ! jpegdec ! autovideosink')
+cam = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink')
 
 while True:
-    _, frame = cam.read()
+    videoComing, frame = cam.read()
+    while not videoComing:
+        print("NO FEED COMING IN")
+        videoComing, frame = cam.read()
 
     cv2.imshow("Vidoe0", frame)
 
