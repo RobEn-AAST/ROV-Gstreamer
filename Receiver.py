20bd6c6e49246b09a3ee5f993cc098c5c3d5d0bc
karim kohel <kareemkohel@gmail.com>
2021-04-17 16:53:09 +0200

    reverted back to single source support for debugging

--- Receiver.py
+++ Receiver.py
@@ -1,32 +1,17 @@
 import cv2
 
-numberOfCams = 2
-
-cams = [ cv2.VideoCapture('udpsrc port=5{}00 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink'.format(i), cv2.CAP_GSTREAMER) for i in range(numberOfCams)]
-
-print("We got past here")
-
-frames = []
+cam = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
 while True:
 
-    for cam in cams:
-        if not cam == None:
-            _, frame = cam.read()
-            frames.append(frame)
+    ret, frame = cam.read()
 
-    for i, frame in enumerate(frames):
-        if not frame == None:
-            cv2.imshow("Frame {}".format(i+3), frame)
-
-    cv2.imshow("Vidoe0", frame)
+    if ret:
+        cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 
-for cam in cams:
-    if not cam == None:
-        cam.release()
-
+cam.release()
 cv2.destroyAllWindows()
