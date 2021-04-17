601bf1e08aa192c674efcdb49f0e9712161c9cbb
karim kohel <kareemkohel@gmail.com>
2021-04-17 22:30:19 +0200

    functioning 2 window recieving script with new camera

--- Receiver.py
+++ Receiver.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 cam0 = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 cam1 = cv2.VideoCapture('udpsrc port=5100 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
@@ -10,13 +11,15 @@ while True:
         ret1, frame1 = cam1.read()
     except Exception:
         break
-    if ret0:
-        cv2.imshow("Frame0", frame0)
-    if ret1:
-        cv2.imshow("Frame1", frame1)
+    # if ret0:
+    #     cv2.imshow("Frame0", frame0)
+    # if ret1:
+    #     cv2.imshow("Frame1", frame1)
 
-    cv2.resizeWindow('Frame0', 640,600)
-    cv2.resizeWindow('Frame1', 640,600)
+    Hori = np.concatenate((frame0, frame1), axis=1)
+    cv2.imshow("frame", Hori)
+    # cv2.resizeWindow('Frame0', 640,600)
+    # cv2.resizeWindow('Frame1', 640,600)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
