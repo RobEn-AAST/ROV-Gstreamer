63278369fef880d0ae1a41e3c7894f157a287568
karim kohel <kareemkohel@gmail.com>
2021-04-20 23:23:06 +0200

    finished resizing and cleaning feed

--- Receiver.py
+++ Receiver.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+# (w, h)
+feedSize = (1480, 840)
+
 cam0 = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 cam1 = cv2.VideoCapture('udpsrc port=5100 ! application/x-rtp, encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
@@ -13,8 +16,11 @@ while True:
         break
 
 
-    Hori = np.concatenate((frame0, frame1), axis=1)
-    cv2.imshow("frame", Hori)
+    collage = np.concatenate((frame0, frame1), axis=1)
+
+    fullSizeFeed = cv2.resize(collage, feedSize)
+
+    cv2.imshow("ROV", fullSizeFeed)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
